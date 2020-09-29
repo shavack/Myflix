0d118f96308768af35edc36efd9d16fb793a31f8
shavack <dkozuch@gmail.com>
2020-09-29 13:28:01 +0200

    first component, data & styles

--- myflix/src/index.js
+++ myflix/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import 'normalize.css'; // gets rid of the default border;
 import App from './app';
+import { GlobalStyles } from './global-styles';
 
-render(<App />, document.getElementById('root'));
+render(<>
+<GlobalStyles/>
+<App />
+</>,
+ document.getElementById('root'));
