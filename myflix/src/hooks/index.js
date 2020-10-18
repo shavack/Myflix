7c80e1575e78d66fc59b028c8650cdb7cdfeae10
shavack <dkozuch@gmail.com>
2020-10-18 22:04:45 +0200

    get film/series data from firebase

--- myflix/src/hooks/index.js
+++ myflix/src/hooks/index.js
@@ -1,1 +1,2 @@
 export { default as UseAuthListener } from './use-auth-listener';
+export { default as useContent } from './use-content';
--- myflix/src/pages/browse.js
+++ myflix/src/pages/browse.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useContent } from '../hooks';
 
 export default function Browse() {
+  const { series } = useContent('series');
+  console.log('series: ', series);
   return (
     <>
       <p>Hello from Browse</p>
